bfdc64e1bbe128c35d779923226ca7279ad69f9c
leandromarten1 <leandromarten1@gmail.com>
2020-10-21 14:44:08 -0300

    Connection / index

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
     response.send();
+});
+
+//app.use('/products', require('./controllers/productController'));
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000!");
 });

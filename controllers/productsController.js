c69752334aa2e5947c86f166250143e154cf7f8b
frederico-campello <fredfmc33@gmail.com>
2020-10-21 16:28:06 -0300

    Requisito 02

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -5,8 +5,27 @@ const router = express.Router();
 
 router.get('/', async (_req, res) => {
   const products = await productsModel.getProducts();
+  res.status(200).json({ products });
+});
 
-  res.json(products);
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await productsModel.getProductById(id);
+
+  if (!product) {
+    return res.status(422).json({ err: { code: 'invalid_data', message: 'Wrong id format' } });
+  }
+  res.status(200).json(product);
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { name, quantity } = req.body;
+    const product = await productsModel.addProduct(name, quantity);
+    res.status(201).json(product);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 module.exports = router;
--- models/productsModel.js
+++ models/productsModel.js
@@ -1,7 +1,22 @@
 require('dotenv').config();
+const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
 const getProducts = async () =>
   connection().then((db) => db.collection('products').find().toArray());
 
-module.exports = { getProducts };
+const getProductById = async (id) => {
+  if (!ObjectId.isValid(id)) return null;
+  const product = await connection().then((db) => db.collection('products').findOne(ObjectId(id)));
+  return product;
+};
+
+const addProduct = async (name, quantity) => {
+  const result = await connection().then((db) =>
+    db.collection('products').insertOne({ name, quantity }),
+  );
+
+  return result.ops[0];
+};
+
+module.exports = { getProducts, getProductById, addProduct };

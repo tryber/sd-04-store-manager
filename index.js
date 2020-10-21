c30ebcc321e4b90ce276a7ff9a444c689ad851c9
frederico-campello <fredfmc33@gmail.com>
2020-10-21 19:37:54 -0300

    Removendo dotenv do index

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const productsController = require('./controllers/productsController');
 
 const app = express();
 
-const port = process.env.PORT;
+const port = 3000;
 
 app.use(bodyParser.json());
 

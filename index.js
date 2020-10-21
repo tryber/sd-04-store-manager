a0c25d6db73b258b3d13540c18bde95e45b84909
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-10-21 20:58:54 -0300

    cc 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const productController = require('./controllers/productController');
+
 const app = express();
 
 app.use(bodyParser.json());
--- models/productModel.js
+++ models/productModel.js
@@ -1,12 +1,10 @@
 const connection = require('./connection');
 
-const getAllProducts = async () => {
-  return connection().then((db) => db.collection('products').find().toArray());
-};
+const getAllProducts = async () =>
+  connection().then((db) => db.collection('products').find().toArray());
 
-const getProductByName = async (item) => {
-  return connection().then((db) => db.collection('products').findOne({ name: { $eq: item } }));
-};
+const getProductByName = async (item) =>
+  connection().then((db) => db.collection('products').findOne({ name: { $eq: item } }));
 
 const addProduct = async (name, quantity) => {
   const result = connection().then((db) => db.collection('products').insertOne({ name, quantity }));

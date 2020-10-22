6dff2fea9c5be9572478871baaa5b319f99906af
igorgbr <igorgphotoarte@gmail.com>
2020-10-22 19:27:10 -0300

    req 1

--- controller/storeController.js
+++ controller/storeController.js
@@ -2,11 +2,38 @@ const dataStore = require('../models/dataModel');
 
 const cadastraProduto = async (req, res) => {
   const { name, quantity } = req.body;
+  console.log(name);
 
-  const product = await dataStore.cadastraProduto(name, quantity);
-  console.log(product);
+  if (name.length <= 5) {
+    res.status(422).json({
+      err: { code: 'invalid_data', message: '"name" length must be at least 5 characters long' },
+    });
+  }
+  if (quantity <= 0) {
+    res.status(422).json({
+      err: { code: 'invalid_data', message: '"quantity" must be larger than or equal to 1' },
+    });
+  }
+  if (typeof quantity === 'string') {
+    res.status(422).json({
+      err: { code: 'invalid_data', message: '"quantity" must be a number' },
+    });
+  }
 
-  res.status(201).json(product);
+  const nameProd = await dataStore.findByName({ name });
+
+  if (nameProd && nameProd.name === name) {
+    res.status(422).json({
+      err: { code: 'invalid_data', message: 'Product already exists' },
+    });
+  }
+
+  {
+    const product = await dataStore.cadastraProduto(name, quantity);
+    console.log(product);
+
+    res.status(201).json(product);
+  }
 };
 
 module.exports = {
--- models/dataModel.js
+++ models/dataModel.js
@@ -1,13 +1,23 @@
 const connection = require('./connection');
 
 const cadastraProduto = async (name, quantity) => {
-  const teste = await connection().then((db) =>
+  const data = await connection().then((db) =>
     db.collection('products').insertOne({ name, quantity }),
   );
-  console.log(teste.ops);
-  return teste.ops;
+
+  return data.ops[0];
+};
+
+const findByName = async (obj) => {
+  const data = await connection()
+    .then((db) => db.collection('products').find(obj).toArray())
+    .then((data) => data.map(({ name }) => ({ name })));
+
+  console.log(data);
+  return data[0];
 };
 
 module.exports = {
   cadastraProduto,
+  findByName,
 };

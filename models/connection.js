f2328e6ba0bc500ddfc449810402d73e195960fb
igorgbr <igorgphotoarte@gmail.com>
2020-10-22 12:15:20 -0300

    perdido...

--- models/connection.js
+++ models/connection.js
@@ -1,11 +1,11 @@
 const mongoClient = require('mongodb').MongoClient;
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
-/* const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager'; */
+/* const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager'; */
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 const DB_NAME = 'StoreManager';
 
-const connection = () => {
+function connection() {
   return mongoClient
     .connect(MONGO_DB_URL, {
       useNewUrlParser: true,
@@ -16,6 +16,6 @@ const connection = () => {
       console.error(err);
       process.exit(1);
     });
-};
+}
 
 module.exports = connection;

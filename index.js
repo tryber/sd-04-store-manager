7bfadad38d9dfaced528feedb12008d6ad198fb3
orlando-messias <programadorlando@gmail.com>
2020-10-21 22:38:15 -0300

    connection, productsModel and index

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
     response.send();
 });
+
+app.listen(port, () => console.log(`listening on port ${port}`));

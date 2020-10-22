4be4cd29f88b6b47bb5a539ea42f1ffa3247a21a
lourenco <loulage@gmail.com>
2020-10-22 19:44:03 -0300

    ccfix

--- models/connection.js
+++ models/connection.js
@@ -7,8 +7,8 @@ const DB_NAME = 'StoreManager';
 // avaliador
 const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
-const connection = () => {
-  return mongoClient
+const connection = () =>
+  mongoClient
     .connect(MONGO_DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -18,6 +18,5 @@ const connection = () => {
       console.error(err);
       process.exit(1);
     });
-};
 
 module.exports = connection;

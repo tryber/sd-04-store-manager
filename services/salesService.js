f9882952ff9ab92d58c5b5a2cd4bf1322e128219
Rangel Paolo <paolorangel1996@outlook.com>
2020-10-23 11:37:04 -0300

    Trailling spaces que nao vi

--- services/salesService.js
+++ services/salesService.js
@@ -50,7 +50,7 @@ const showASpecificSaleById = async (id) => {
 
 const deleteASale = async (id) => {
   const saleResultOfDeletion = await Sales.deleteSaleById(id);
-  
+
   if (saleResultOfDeletion === null) {
     return objectErro;
   }

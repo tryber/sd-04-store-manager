87367d0560e1508a1093b6da12238395b3d242a3
danilossousa <danilosantossousa@gmail.com>
2020-10-26 19:04:05 -0300

    v0.1.0.7

--- models/connection.js
+++ models/connection.js
@@ -1,26 +1,26 @@
 const { host, database } = require('./config');
 const mongoClient = require('mongodb').MongoClient;
 
-// let schema;
-// const connection = () =>
-//   (schema
-//     ? Promise.resolve(schema)
-//     : mongoClient.connect(
-//         host, {
-//           useNewUrlParser: true,
-//           useUnifiedTopology: true,
-//         },
-//       )
-//       .then((conn) => conn.db(database))
-//       .catch((_err) => process.exit(1))
-//   );
-
+let schema;
 const connection = () =>
-  mongoClient.connect(host, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then((conn) => conn.db(database))
-  .catch((_err) => process.exit(1));
+  (schema
+    ? Promise.resolve(schema)
+    : mongoClient.connect(
+        host, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        },
+      )
+      .then((conn) => conn.db(database))
+      .catch((_err) => process.exit(1))
+  );
+
+// const connection = () =>
+//   mongoClient.connect(host, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then((conn) => conn.db(database))
+//   .catch((_err) => process.exit(1));
 
 module.exports = connection;

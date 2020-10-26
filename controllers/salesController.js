75e546fec3718048fd95fb339e5f283eea8547ee
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-10-26 11:02:28 -0300

    cc

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,9 +1,11 @@
-// const express = require('express');
-// const { checkProduct } = require('../helper/productSchema');
-// const productModel = require('../models/productModel');
+const express = require('express');
+const salesModel = require('../models/salesModel');
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.post('/', async (req, res) => {
-    
-//   });
+router.get('/', async (req, res) => {
+  const allSales = await salesModel.getAllSales();
+  res.status(200).json({ sales: allSales });
+});
+
+module.exports = router;
--- models/salesModel.js
+++ models/salesModel.js
@@ -1,9 +1,14 @@
 const connection = require('./connection');
-const { ObjectID } = require('mongodb');
+// const { ObjectID } = require('mongodb');
 
-const registerSale = async (id, quantity) => {
-  const result = await connection().then((db) =>
-    db.collection('sales').insertMany([{ id, quantity }]),
-  );
-  return result.ops[0];
-};
+const getAllSales = async () =>
+  connection().then((db) => db.collection('sales').find().toArray());
+
+// const registerSale = async (id, quantity) => {
+//   const result = await connection().then((db) =>
+//     db.collection('sales').insertMany([{ id, quantity }]),
+//   );
+//   return result.ops[0];
+// };
+
+module.exports = getAllSales;

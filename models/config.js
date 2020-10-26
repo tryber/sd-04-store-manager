550b6a2d2aff037ab9ee0d8a41a3dddfca10a9ef
danilossousa <danilosantossousa@gmail.com>
2020-10-26 19:27:40 -0300

    v0.1.0.8

--- models/config.js
+++ models/config.js
@@ -1,9 +1,9 @@
-require('dotenv/config');
+// require('dotenv/config');
 
-module.exports = {
-  host: process.env.MONGO_DB_URL,
-  database: process.env.DB_NAME,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASS,
-  // port: process.env.DB_PORT,
-};
+// module.exports = {
+//   host: process.env.MONGO_DB_URL,
+//   database: process.env.DB_NAME,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   port: process.env.DB_PORT,
+// };
--- models/connection.js
+++ models/connection.js
@@ -1,26 +1,37 @@
-const { host, database } = require('./config');
 const mongoClient = require('mongodb').MongoClient;
+// const { host, database } = require('./config');
+
+const DB_NAME = 'StoreManager';
+
+// Local
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+
+// Online
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 let schema;
 const connection = () =>
   (schema
     ? Promise.resolve(schema)
     : mongoClient.connect(
-        host, {
+        MONGO_DB_URL, {
           useNewUrlParser: true,
           useUnifiedTopology: true,
         },
       )
-      .then((conn) => conn.db(database))
+      .then((conn) => {
+        schema = conn.db(DB_NAME);
+        return schema;
+      })
       .catch((_err) => process.exit(1))
   );
 
 // const connection = () =>
-//   mongoClient.connect(host, {
+//   mongoClient.connect(MONGO_DB_URL, {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true,
 //   })
-//   .then((conn) => conn.db(database))
+//   .then((conn) => conn.db(DB_NAME))
 //   .catch((_err) => process.exit(1));
 
 module.exports = connection;

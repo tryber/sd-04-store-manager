cec1a08a8ee183ecc51dac13820dea3a07a3b14f
juniomelos <juniomelos@gmail.com>
2020-10-28 15:21:43 -0300

    connection with process.env

--- models/connection.js
+++ models/connection.js
@@ -1,17 +1,8 @@
-/* eslint-disable prefer-destructuring */
-require('dotenv').config();
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager'; // local
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager'; // evaluator
-
-const {
-  MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager',
-} = process.env;
 const { DB_NAME } = process.env;
-
-// const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
-// const DB_NAME = process.env.DB_NAME;
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
 
 let schema;
 

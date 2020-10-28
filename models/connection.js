7242c05a8c891c9c12b979137a2d536a642c9329
lourenco <loulage@gmail.com>
2020-10-28 15:11:21 -0300

    connect fix

--- models/connection.js
+++ models/connection.js
@@ -2,10 +2,10 @@ const mongoClient = require('mongodb').MongoClient;
 
 const DB_NAME = 'StoreManager';
 // local
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 
 // avaliador
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 const connection = () =>
   mongoClient

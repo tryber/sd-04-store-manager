25b89c6e5c87bebedad8dc374bc576e3fe5cde65
Pedro <pedrotpoliveira@gmail.com>
2020-10-27 21:27:34 -0300

    Delete Sales - Eval

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -16,6 +16,15 @@ router.get('/', async (req, res) => {
   res.status(200).json({ sales: salesArray });
 });
 
+router.get('/:id', rescue(async (req, res) => {
+  const { id } = req.params;
+  const sale = await salesService.getById(id);
+  if (!sale) {
+    throw new Error('Sale not found');
+  }
+  res.status(200).json(sale);
+}));
+
 router.post('/', rescue(async (req, res) => {
   const items = req.body;
   const result = await salesService.add(items);
@@ -44,19 +53,19 @@ router.delete('/:id', rescue(async (req, res) => {
   } catch (e) {
     throw new SalesErr('Wrong sale ID format');
   }
-  const sale = await salesService.getById(id);
-  if (!sale) {
-    return res.status(404)
-      .json({ err: { code: 'not_found', message: 'Sale not found' } });
-  }
   await salesService.del(id);
 
-  res.status(200).json(sale);
+  res.status(200).json({ status: 'sale deleted' });
 }));
 
 router.use(rescue.from(SalesErr, (error, _req, res, _next) => {
   res.status(422)
     .json({ err: { code: 'invalid_data', message: error.message } });
 }));
+
+router.use((err, req, res, _next) => {
+  res.status(404)
+    .json({ err: { code: 'not_found', message: err.message } });
+});
 
 module.exports = router;

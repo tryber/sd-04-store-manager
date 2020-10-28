005e482d246dd6c81783878a37d7cfb7fce0ee11
danilossousa <danilosantossousa@gmail.com>
2020-10-28 17:11:03 -0300

    v0.5.0.7

--- middlewares/salesValidations.js
+++ middlewares/salesValidations.js
@@ -10,15 +10,16 @@ const errors = {
   },
 };
 
+const resp = (type, nErr) => type.status(422).json(errors[nErr]); // CC
+
 const createSalesVal = async (req, res, next) => {
   const pendencies = [];
 
   for (let i = 0, len = req.body.length; i < len; i += 1) {
-    const resp = (nErr) => res.status(422).json(errors[nErr]);
     const id = req.body[i].productId;
     const qtt = req.body[i].quantity;
 
-    if (!ObjectId.isValid(id) || qtt <= 0 || !Number.isInteger(qtt)) return resp(1);
+    if (!ObjectId.isValid(id) || qtt <= 0 || !Number.isInteger(qtt)) return resp(res, 1);
 
     pendencies.push(productsModel.readById(id));
   }
@@ -26,7 +27,7 @@ const createSalesVal = async (req, res, next) => {
   const products = await Promise.all(pendencies);
 
   for (let i = 0, len = products.length; i < len; i += 1) {
-    if (!products[i]) return resp(1);
+    if (!products[i]) return resp(res, 1);
   }
 
   next();

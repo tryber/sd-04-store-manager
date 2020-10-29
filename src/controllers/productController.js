2c30f1f37248b7c18c16043a0b2da07a54883875
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-10-29 00:08:52 -0300

    fix http status

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -9,7 +9,7 @@ router.post('/', validateProduct, async (req, res) => {
     const { name, quantity } = req.body;
     const product = await productModel.addProduct(name, quantity);
 
-    res.status(200).json(product);
+    res.status(201).json(product);
   } catch (_e) {
     console.log(_e);
     res.status(501).json({ message: 'Falha ao cadastrar o produto!' });

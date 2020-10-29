d9731e64f106512559a71c7be4e47420170d6945
Filipe de Castro <filipeyoga@gmail.com>
2020-10-29 14:10:47 -0300

    style: CC

--- models/productModel.js
+++ models/productModel.js
@@ -1,6 +1,6 @@
 const connection = require('./connection');
 
-const addProduct = async (name, quantity) => {
+module.addProduct = async (name, quantity) => {
   try {
     const db = await connection();
     const newProduct = await db.collection('products').insertOne({ name, quantity });
@@ -10,7 +10,7 @@ const addProduct = async (name, quantity) => {
   }
 };
 
-const listProducts = async () => {
+module.listProducts = async () => {
   try {
     const db = await connection();
     return await db.collection('products').find({}).toArray();
@@ -19,5 +19,3 @@ const listProducts = async () => {
     return [];
   }
 };
-
-module.exports = { addProduct, listProducts };

01992498e5867ef4ee3f46b054fb880ed7173d23
caiolmf <caio.lmf@gmail.com>
2020-10-30 12:39:26 -0300

    code climate

--- controllers/sales.js
+++ controllers/sales.js
@@ -1,11 +1,8 @@
 const salesModel = require('../model/sales');
 
-const createSale = async (req, res) => {
-  const sales = req.body;
-  salesModel
-    .create(sales)
-    .then((result) => res.status(201).json(result))
-    .catch(() => res.status(422).send({ err: { message: 'Error', code: 'invalid_data' } }));
+const createSale = async ({ body }, res) => {
+  const sales = body;
+  salesModel.create(sales).then((result) => res.status(201).json(result));
 };
 
 module.exports = { createSale };

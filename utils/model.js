2b19ed3aaded61dac7cac218a4a30d5156941bbf
Tiago-ers <trsilva284@gmail.com>
2020-10-30 10:22:19 -0300

    cc - refatoracao 1

---
+++ utils/model.js
@@ -0,0 +1,39 @@
+const { ObjectId } = require('mongodb');
+const connection = require('../models/connection');
+
+/**
+ * Este arquivo é uma refatoração
+ * das funções que podem ser usadas tanto
+ * na model productModel quanto na salesModel
+ */
+
+/**
+ * ANCHOR Delete
+ *
+ * @param id
+ * @param collection
+ */
+const remove = async (id, collection) => {
+  if (!ObjectId.isValid(id)) return null; // Verifica se o Id é válido
+  await connection().then((db) => db.collection(collection).deleteOne({ _id: ObjectId(id) }));
+};
+
+/**
+ * ANCHOR Find All products
+ */
+const findAll = async (collection) => {
+  return connection().then((db) => db.collection(collection).find().toArray());
+};
+
+/**
+ * ANCHOR Find product by id
+ *
+ * @param id
+ */
+const findById = async (id, collection) => {
+  if (!ObjectId.isValid(id)) return null; // Verifica se o Id é válido
+
+  return connection().then((db) => db.collection(collection).findOne(ObjectId(id)));
+};
+
+module.exports = { remove, findAll, findById };

aaf6717ec80bf93a3924eb91a39c01db43e976be
Mateus-Moreira <ashuraw@hotmail.com>
2020-10-31 14:10:00 -0200

    0.1

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const router = express.Router();
 const ProductsServices = require('../services/productsServices');
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ProductsController = require('./controllers/productsController');
+
 const app = express();
 const port = 3000;
 

c33f3ccf99a9102602997d14f3c0204ad0a16a37
Mateus-Moreira <ashuraw@hotmail.com>
2020-10-31 13:29:50 -0200

    0.1

--- models/connection.js
+++ models/connection.js
@@ -4,15 +4,15 @@ const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 const connection = () =>
-    mongoClient
-        .connect(MONGO_DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then((conn) => conn.db(DB_NAME))
-        .catch((err) => {
-            console.error(err);
-            process.exit(1);
-        });
+  mongoClient
+    .connect(MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
 
 module.exports = connection;

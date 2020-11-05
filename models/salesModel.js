7572d1d6e6548c8aef805d90985a476a03701989
Victor L <victor.lustosacp@gmail.com>
2020-11-05 15:46:30 -0300

    cc

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,10 +1,4 @@
 const connection = require('./connection');
-const { ObjectId, connect } = require('mongodb');
-
-const getAll = async () => connection().then((db) => db.collection('products').find({}).toArray());
-
-const findByName = async (collection, name) =>
-  connection().then((db) => db.collection(collection).findOne({ name }));
 
 const addSales = async (collections, sales) => {
   const result = connection().then((db) => db.collection(collections).insertOne(sales));
@@ -12,19 +6,5 @@ const addSales = async (collections, sales) => {
 };
 
 // Aprender sobre ObjectId
-const findById = async (collection, id) => {
-  if (!ObjectId.isValid(id)) return null;
-  return connection().then((db) => db.collection(collection).findOne(ObjectId(id)));
-};
 
-const update = async (collection, id, elements) => {
-  connection().then((db) =>
-    db.collection(collection).updateOne({ _id: ObjectId(id) }, { $set: elements }),
-  );
-};
-
-const deleteProduct = async (collection, id) => {
-  connection().then((db) => db.collection(collection).deleteOne({ _id: ObjectId(id) }));
-};
-
-module.exports = { findByName, addSales, findById, getAll, update, deleteProduct };
+module.exports = { addSales };

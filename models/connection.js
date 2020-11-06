3b6c261ac45c211633cb616fb07c9457389a88f8
Victor L <victor.lustosacp@gmail.com>
2020-11-06 15:52:23 -0300

    cc

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 // const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 const DB_NAME = 'StoreManager';
-
 const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 const connection = () =>

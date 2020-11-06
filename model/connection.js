34cc982b3ee486c7d4e919497acba30f082a0f74
RobertoAsgon <robertoasgon@gmail.com>
2020-11-06 17:45:19 -0400

    test

--- model/connection.js
+++ model/connection.js
@@ -8,7 +8,7 @@
 
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 
 const connection = () =>
   MongoClient.connect(MONGO_DB_URL, {
--- model/sale.js
+++ model/sale.js
@@ -1,22 +1,22 @@
 const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
-const getAllSales = async () => connection()
+const getAllSales = () => connection()
   .then((db) => db.collection('sales').find({}).toArray());
 
-const postNewSale = async (itensSold) => connection()
+const postNewSale = (itensSold) => connection()
   .then((db) => db.collection('sales').insertOne({ itensSold }))
   .then(({ insertedId }) => ({ _id: insertedId, itensSold }));
 
-const getSaleById = async (id) => connection()
+const getSaleById = (id) => connection()
   .then((db) => db.collection('sales').findOne({ _id: ObjectId(id) }));
 
-const putSale = async (id, itensSold) => connection()
+const putSale = (id, itensSold) => connection()
   .then((db) => db.collection('sales')
     .updateOne({ _id: ObjectId(id) }, { $set: { itensSold } }))
   .then(() => ({ _id: id, itensSold }));
 
-const deleteSale = async (id) => connection()
+const deleteSale = (id) => connection()
   .then((db) => db.collection('sales').deleteOne({ _id: ObjectId(id) }));
 
 module.exports = {

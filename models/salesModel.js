0c17ffae10d82a2eea3c5b3f8656c777071a625e
deboracosilveira <deboracosilveira@gmail.com>
2020-11-06 14:56:19 -0300

    Req 5 - teste 4

--- models/salesModel.js
+++ models/salesModel.js
@@ -2,8 +2,11 @@
 const connection = require('./connection');
 
 const addSale = async (itensSold) => {
-  const result = await connection().then((db) => db.collection('sales').insertOne({ itensSold }));
-  return result.ops[0];
+  const sales = await connection().then((db) => db.collection('sales').insertOne({ itensSold }));
+  const { insertedId: _id } = sales;
+
+  const result = { _id, itensSold };
+  return result;
 };
 
 module.exports = {

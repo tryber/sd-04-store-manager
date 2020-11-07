413164620f04fa14a4e05a3edb0f5dfec335ede1
Mateus-Moreira <ashuraw@hotmail.com>
2020-11-07 00:06:42 -0300

    storeManager 0.3

--- models/connection.js
+++ models/connection.js
@@ -1,14 +1,23 @@
 const mongoClient = require('mongodb').MongoClient;
-
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-
-const connection = () =>
-  mongoClient
+let schema = null;
+//const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+async function connection() {
+  if (schema) return Promise.resolve(schema);
+  return mongoClient
     .connect(MONGO_DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then((conn) => conn.db('StoreManager'))
-    .catch(() => process.exit(1));
-
+    .then((conn) => conn.db(DB_NAME))
+    .then((dbSchema) => {
+      schema = dbSchema;
+      return schema;
+    })
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+}
 module.exports = connection;

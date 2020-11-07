3dbe8bfa97b4540fbad9bf9f19d741a060f7fe91
Mateus-Moreira <ashuraw@hotmail.com>
2020-11-07 08:21:52 -0300

    storeManager 0.3

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 const mongoClient = require('mongodb').MongoClient;
+
 let schema = null;
-//const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 async function connection() {
   if (schema) return Promise.resolve(schema);

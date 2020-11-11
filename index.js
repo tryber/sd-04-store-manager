7eecd16eae445ff447867fae718422f6539d8309
Thiago Tavares <thiagooo15@gmail.com>
2020-11-11 17:32:52 -0300

    testando

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// const productsController = require('./controllers/productsController');
+
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
   response.send();
 });
+
+// app.use('/products', productsController);
 
 app.listen(port, () => console.log(`Listening on ${port}`));

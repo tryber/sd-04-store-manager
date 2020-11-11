88a20868e9e9132a567ab5b131acdc6468259b41
Thiago Tavares <thiagooo15@gmail.com>
2020-11-11 17:20:04 -0300

    spot freez

--- controllers/salesController.js
+++
@@ -1,9 +0,0 @@
-const SalesModel = require('../models/salesModel');
-
-const listSales = async (req, res) => {
-  const sale = await SalesModel.getAll();
-
-  res.status(200).jsaon(sale);
-};
-
-module.exports = listSales;
--- models/salesModel.js
+++
@@ -1,5 +0,0 @@
-const connection = require('./connection');
-
-const getAll = async () => connection().then((db) => db.collection('sales').find().toArray());
-
-module.exports = { getAll };

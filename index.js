fca0b83d22ca85bb902d80ab85b92aa420eac720
Kaecio <kaecio@pop-os.localdomain>
2020-11-13 17:37:16 -0300

    Testando CC,123,123, testando, testando

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const bodyparse = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true})); // <- se os dados via POST vier do HTML
+app.use(bodyParser.urlencoded({ extended:true })); // <- se os dados via POST vier do HTML
 app.use(bodyParser.json); // <- se os dados via POST vier da URL ( blabla/lele )
-
 
 // não remova esse endpoint, e para o avaliador funcionar *
 app.get('/', (request, response) => {
     response.send();
 });
-
-
 
 app.listen(3000,()=>{
     console.log('Rodando na porta 3000 liso...')

8a64b7bb97c94d560715d138a62af6d15101af93
Kaecio <kaecio@pop-os.localdomain>
2020-11-13 17:32:29 -0300

    Tenho que começar por um lugar, vc foi oescolhido index.js

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-// não remova esse endpoint, e para o avaliador funcionar
+const bodyParser = require('body-parser');
+const express = require('express');
+const bodyparse = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true})); // <- se os dados via POST vier do HTML
+app.use(bodyParser.json); // <- se os dados via POST vier da URL ( blabla/lele )
+
+
+// não remova esse endpoint, e para o avaliador funcionar *
 app.get('/', (request, response) => {
     response.send();
 });
+
+
+
+app.listen(3000,()=>{
+    console.log('Rodando na porta 3000 liso...')
+}) // escutando na porta 3000 ....

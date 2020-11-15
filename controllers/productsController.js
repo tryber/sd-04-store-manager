aa755acc3fb214bfac9701758b727c536de3b676
diego <diegodiko05@gmail.com>
2020-11-15 20:47:00 -0300

    Requisito 4

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -47,4 +47,10 @@ router.put(
   },
 );
 
+router.delete('/:id', validations.verifyProductById, async (req, res) => {
+  const { id } = req.params;
+  await crudModel.remove('products', id);
+  res.status(200).json(req.product);
+});
+
 module.exports = router;

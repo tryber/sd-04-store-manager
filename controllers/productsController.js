8c7e2fd6ca764dd1de924b00b77d48ec8b3150b0
Luma Arruda <luma.oarruda@gmail.com>
2020-11-16 18:08:12 -0300

    requisitos de produtos 1 - 4

---
+++ controllers/productsController.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const productsModel = require('../models/productsModel')
+const productsValidations = require('../middlewares/productsValidation');
+
+const router = express.Router();
+
+router.post(
+  '/', 
+  productsValidations.nameLengthValidation,
+  productsValidations.nameExistenceValidation,
+  productsValidations.quantityValidation,
+  productsValidations.isNumberValidation,
+  async (req, res) => {
+    try {
+      const { name, quantity } = req.body;
+      const product = await productsModel.addProduct(name, quantity);
+
+      res.status(201).json(product);
+    } catch (_e) {
+      res.status(501).json({ message: 'Sorry, something went wrong :(' });
+    }
+  }
+);
+
+router.get('/', async (_req, res) => {
+  try {
+    const products = await productsModel.findAll();
+  
+    res.status(200).json({ products })
+  } catch (_e) {
+    res.status(501).json({ message: 'Sorry, something went wrong :(' });
+  }
+});
+
+router.get('/:id', productsValidations.idExistsValidation, async (req, res) => {
+  try {
+    const product = req.product;
+
+    res.status(200).json(product);
+  } catch (_e) {
+    res.status(501).json({ message: 'Sorry, something went wrong :(' });
+  }
+});
+
+router.put(
+  '/:id', 
+  productsValidations.nameLengthValidation,
+  productsValidations.quantityValidation,
+  productsValidations.isNumberValidation,
+  async (req, res) => {
+    try {
+      const { name, quantity } = req.body;
+      const { id } = req.params;
+
+      await productsModel.updateProduct(id, name, quantity);
+
+      const updatedProduct = await productsModel.findById(id);
+
+      res.status(200).json(updatedProduct);
+    } catch (_e) {
+      res.status(501).json({ message: 'Sorry, something went wrong :(' });
+    }
+  }
+);
+
+router.delete('/:id', productsValidations.idExistsValidation, async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await productsModel.deleteProduct(id);
+
+    const deletedProduct = req.product;
+
+    res.status(200).json(deletedProduct);
+  } catch (_e) {
+    res.status(501).json({ message: 'Sorry, something went wrong :(' });
+  }
+});
+
+module.exports = router;
---
+++ middlewares/productsValidation.js
@@ -0,0 +1,69 @@
+const productsModel = require('../models/productsModel');
+
+const buildResponse = (code, message) => {
+  const resp = { err: { code, message } };
+  return resp;
+};
+
+const nameLengthValidation = (req, res, next) => {
+  const { name } = req.body;
+  if (name.length < 5) {
+    return res
+      .status(422)
+      .json(buildResponse('invalid_data', '"name" length must be at least 5 characters long'));
+  }
+  next();
+};
+
+const nameExistenceValidation = async (req, res, next) => {
+  const { name } = req.body;
+  const product = await productsModel.findByName(name);
+
+  if (product) {
+    return res.status(422).json(buildResponse('invalid_data', 'Product already exists'));
+  }
+
+  next();
+};
+
+const quantityValidation = (req, res, next) => {
+  const { quantity } = req.body;
+  if (quantity <= 0) {
+    return res
+      .status(422)
+      .json(buildResponse('invalid_data', '"quantity" must be larger than or equal to 1'));
+  }
+
+  next();
+};
+
+const isNumberValidation = (req, res, next) => {
+  const { quantity } = req.body;
+  // Referencia: isNaN Stack Overflow - https://bityli.com/JOfue
+  if (isNaN(quantity)) {
+    return res.status(422).json(buildResponse('invalid_data', '"quantity" must be a number'));
+  }
+
+  next();
+};
+
+const idExistsValidation = async (req, res, next) => {
+  const { id } = req.params;
+  const product = await productsModel.findById(id);
+
+  if (!product) {
+    return res.status(422).json(buildResponse('invalid_data', 'Wrong id format'));
+  }
+
+  req.product = product;
+
+  next();
+};
+
+module.exports = {
+  nameLengthValidation,
+  nameExistenceValidation,
+  quantityValidation,
+  isNumberValidation,
+  idExistsValidation,
+};

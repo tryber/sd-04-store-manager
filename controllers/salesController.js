b60591c9c6a818b8fdf2a1fd2d09f50d59202486
Thiago Tavares <thiagooo15@gmail.com>
2020-11-16 20:43:08 -0300

    req 6 done

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -11,12 +11,34 @@ router.post('/', async (req, res) => {
   return res.status(200).json(sale);
 });
 
-// router.post('/', async (req, res) => {
+router.get('/', async (req, res) => {
+  const sale = await saleService.getAll();
+  res.status(200).json(sale);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const sale = await saleService.getById(id);
+
+  if (sale.error) return res.status(404).json({ err: sale.err });
+  res.status(200).json(sale);
+});
+
+// router.put('/:id', async (req, res) => {
+//   const { id } = req.params;
 //   const { name, quantity } = req.body;
-//   const product = await ProductsServices.createProduct(name, quantity);
+//   const product = await ProductsServices.updateProduct(id, name, quantity);
 
 //   if (product.error) return res.status(422).json({ err: product.err });
-//   return res.status(201).json(product);
+//   return res.status(200).json(product);
+// });
+
+// router.delete('/:id', async (req, res) => {
+//   const { id } = req.params;
+//   const product = await ProductsServices.deleteProduct(id);
+
+//   if (product.error) return res.status(422).json({ err: product.err });
+//   res.status(200).json(product);
 // });
 
 module.exports = router;
--- services/salesService.js
+++ services/salesService.js
@@ -4,7 +4,10 @@ const productsModel = require('../models/productsModel');
 
 const createSale = async (item) => {
   const sale = await salesModel.addSale(item);
-  const err = { err: { code: 'invalid_data', message: 'Wrong product ID or invalid quantity' }, error: true };
+  const err = {
+    err: { code: 'invalid_data', message: 'Wrong product ID or invalid quantity' },
+    error: true,
+  };
 
   const { productId, quantity } = sale.itensSold[0];
   await productsModel.updateProduct(productId, quantity);
@@ -13,6 +16,53 @@ const createSale = async (item) => {
   return sale;
 };
 
-const getAll = async () => ({ sales: await salesModel.findAll() });
+const getAll = async () => {
+  const sales = await salesModel.findAll();
+  if (!sales) return null;
+  return { sales };
+};
 
-module.exports = { getAll, createSale };
+const getById = async (id) => {
+  const sale = await salesModel.findById(id);
+  const err = { err: { code: 'not_found', message: 'Sale not found' }, error: true };
+
+  if (!sale) return err;
+  return sale;
+};
+
+// const getById = async (id) => {
+//   const product = await productsModel.findById(id);
+//   const err = { err: { code: 'invalid_data', message: 'Wrong id format' }, error: true };
+
+//   if (!product) return err;
+//   return product;
+// };
+
+//   const product = await productsModel.addProduct(name, quantity);
+//   return { _id: product.insertedId, name, quantity };
+// };
+
+// const updateProduct = async (id, name, quantity) => {
+//   const err = await isValid(name, quantity);
+//   if (err.message) return { err, error: true };
+
+//   const product = await productsModel.updateProduct(id, name, quantity);
+//   return { _id: product.insertedId, name, quantity };
+// };
+
+// const deleteProduct = async (id) => {
+//   const product = await productsModel.findById(id);
+//   const err = { err: { code: 'invalid_data', message: 'Wrong id format' }, error: true };
+
+//   await productsModel.deleteProduct(id);
+//   if (!product) return err;
+//   return product;
+// };
+
+module.exports = {
+  createSale,
+  // updateSale,
+  // deleteSale,
+  getAll,
+  getById,
+};

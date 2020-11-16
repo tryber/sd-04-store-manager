1650f6cab8f8c86e9ab65cc91738ade6687f85e8
diego <diegodiko05@gmail.com>
2020-11-16 18:28:48 -0300

    Fix: Passando nome correto do banco

--- service/quantityService.js
+++ service/quantityService.js
@@ -1,8 +1,8 @@
 const crudModel = require('../models/crudModel');
 
 const updateQuantity = async (action, productId, quantity) => {
-  const product = await crudModel.findById(productId);
-  console.log('product by id', product);
+  const product = await crudModel.findById('products', productId);
+  console.log('productId', productId);
   if (!product) return;
   let newQuantity;
   if (action === 'POST') {

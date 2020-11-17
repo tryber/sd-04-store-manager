cb5b1aa521763ca1c99de5dc727540daa28426a1
Thiago Tavares <thiagooo15@gmail.com>
2020-11-17 20:14:11 -0300

    ore 1 and done

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -23,28 +23,22 @@ router.get('/:id', async (req, res) => {
   if (sale.error) return res.status(404).json({ err: sale.err });
   res.status(200).json(sale);
 });
-// ------------------------------------------------------------------------------------
-//  req 7
 
 router.put('/:id', async (req, res) => {
   const { id } = req.params;
   const saleUpdate = req.body;
-  console.log(req.body);
   const sale = await saleService.updateSale(id, saleUpdate);
 
   if (sale.error) return res.status(422).json({ err: sale.err });
   return res.status(200).json(sale);
 });
 
-// aqui acaba o req 7
-// --------------------------------------------------------------------------------------
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const sale = await saleService.deleteSale(id);
 
-// router.delete('/:id', async (req, res) => {
-//   const { id } = req.params;
-//   const product = await ProductsServices.deleteProduct(id);
-
-//   if (product.error) return res.status(422).json({ err: product.err });
-//   res.status(200).json(product);
-// });
+  if (!sale) return res.status(422).json({ err: sale.err });
+  res.status(200).json(sale);
+});
 
 module.exports = router;
--- services/salesService.js
+++ services/salesService.js
@@ -1,17 +1,6 @@
 const salesModel = require('../models/salesModel');
 const productsModel = require('../models/productsModel');
 
-// const isValid = async (name, quantity) => {
-//   const err = { code: 'invalid_data' };
-
-//   if (name.length < 5) err.message = '"name" length must be at least 5 characters long';
-//   if (await productsModel.findByName(name)) err.message = 'Product already exists';
-//   if (quantity < 1 || quantity < 0) err.message = '"quantity" must be larger than or equal to 1';
-//   if (typeof quantity !== 'number') err.message = '"quantity" must be a number';
-
-//   return err;
-// };
-
 const createSale = async (item) => {
   const sale = await salesModel.addSale(item);
   const err = {
@@ -26,7 +15,6 @@ const createSale = async (item) => {
   return sale;
 };
 
-// ta errado, arrumar pra passar o req 7 depois!
 const updateSale = async (id, saleUpdate) => {
   try {
     const sale = await salesModel.updateSale(id, saleUpdate);
@@ -48,14 +36,6 @@ const updateSale = async (id, saleUpdate) => {
   }
 };
 
-// const updateProduct = async (id, name, quantity) => {
-//   const err = await isValid(name, quantity);
-//   if (err.message) return { err, error: true };
-
-//   const product = await productsModel.updateProduct(id, name, quantity);
-//   return { _id: product.insertedId, name, quantity };
-// };
-
 const getAll = async () => {
   const sales = await salesModel.findAll();
   if (!sales) return null;
@@ -70,19 +50,24 @@ const getById = async (id) => {
   return sale;
 };
 
-// const deleteProduct = async (id) => {
-//   const product = await productsModel.findById(id);
-//   const err = { err: { code: 'invalid_data', message: 'Wrong id format' }, error: true };
+const deleteSale = async (id) => {
+  const sale = await salesModel.findById(id);
+  const err = { err: { code: 'invalid_data', message: 'Wrong sale ID format' }, error: true };
 
-//   await productsModel.deleteProduct(id);
-//   if (!product) return err;
-//   return product;
-// };
+  if (!sale) return err;
+
+  await salesModel.deleteSale(id);
+
+  const { productId, quantity } = sale.itensSold[0];
+  await productsModel.updateProduct(productId, -quantity);
+
+  return sale;
+};
 
 module.exports = {
   createSale,
   updateSale,
-  // deleteSale,
+  deleteSale,
   getAll,
   getById,
 };

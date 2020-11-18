731071834891dc503d3582864acbcb0ff85ad4cc
matheusysd <matheusyurisd@gmail.com>
2020-11-18 00:11:59 -0300

    Setting .get response

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,4 +15,9 @@ router.post('/', validations.validateName, verifyIfExists, validateQuantity, asy
   }
 });
 
+router.get('/', async (_req, res) => {
+  const products = await crudModel.findAll('products');
+  res.status(200).json(products);
+});
+
 module.exports = router;

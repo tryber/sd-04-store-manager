1d04ad89b9372c06b3cf13d15f76bde87a7c7108
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2020-11-19 11:20:55 -0300

    URL

--- models/connection.js
+++ models/connection.js
@@ -3,8 +3,8 @@ const mongoClient = require('mongodb').MongoClient;
 const DB_NAME = 'StoreManager';
 
 // Avaliador
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 
 const connect = () =>
   mongoClient

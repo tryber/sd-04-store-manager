63c0c7b94c5c31a727fbd94f00cd8a0c285908ae
matheusysd <matheusyurisd@gmail.com>
2020-11-19 00:35:58 -0300

    setting upp /products route

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-// não remova esse endpoint, e para o avaliador funcionar
+const express = require('express');
+const productsController = require('./controllers/productsController');
+const salesController = require('./controllers/salesController');
+
+const app = express();
+app.use(express.json());
+app.use('/products', productsController);
+// app.use('sales', salesController);
 app.get('/', (request, response) => {
-    response.send();
+  response.send();
 });
-//commit
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));

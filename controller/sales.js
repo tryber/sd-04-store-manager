1bd907d7fb6dda343b978c6c8f1b6d6704a787f4
procopio420 <lucas-procopio@outlook.com>
2020-11-19 14:30:34 -0300

    fix: fix last validation

--- controller/sales.js
+++ controller/sales.js
@@ -1,58 +1,63 @@
-// const express = require('express');
-// const validations = require('../middlewares/inputValidations');
-// const crudModel = require('../model');
-// const quantityService = require('../service/quantityService');
+const express = require('express');
+const validate = require('../middleware/validate');
+const crud = require('../model');
+const quantityService = require('../service/quantity');
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.post(
-//   '/',
-//   validations.validateSalesQuantity,
-//   validations.validateQuantityIsNumber,
-//   async (req, res) => {
-//     const [...itensSold] = req.body;
-//     const sales = await crudModel.addSale(itensSold);
-//     await quantityService.updateProductQuantity(req.method, itensSold);
-//     res.status(200).json(sales);
-//   },
-// );
+router.post(
+  '/',
+  validate.salesQuantityIsNumber,
+  validate.salesQuantityIsNot0OrLess,
+  validate.quantityIsNumber,
+  validate.quantityIsNot0OrLess,
+  async (req, res) => {
+    const [...itensSold] = req.body;
+    const sales = await crud.create('sales', { itensSold });
+    await quantityService.updateProductQuantity(req.method, itensSold);
+    res.status(200).json(sales);
+  },
+);
 
-// router.get('/', async (_req, res) => {
-//   crudModel.findAll('sales').then((sales) => res.status(200).json({ sales }));
-// });
+router.get('/', async (_req, res) => {
+  crud.read('all', null, 'sales').then((sales) => res.status(200).json({ sales }));
+});
 
-// router.get('/:id', async (req, res) => {
-//   const { id } = req.params;
-//   const sale = await crudModel.findById('sales', id);
-//   if (!sale) {
-//     return res.status(404).json({ err: { code: 'not_found', message: 'Sale not found' } });
-//   }
-//   res.status(200).json(sale);
-// });
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const sale = await crud.read('id', id, 'sales');
+  if (!sale) {
+    return res.status(404).json({ err: { code: 'not_found', message: 'Sale not found' } });
+  }
+  res.status(200).json(sale);
+});
 
-// router.put(
-//   '/:id',
-//   validations.validateSales,
-//   validations.validateSalesQuantity,
-//   validations.validateQuantityIsNumber,
-//   validations.verifySaleById,
-//   async (req, res) => {
-//     const { id } = req.params;
-//     const document = {
-//       itensSold: req.body,
-//     };
-//     await crudModel.update('sales', id, document);
-//     crudModel.findById('sales', id).then((sale) => res.status(200).json(sale));
-//   },
-// );
+router.put(
+  '/:id',
+  validate.salesProductIdAndQuantity,
+  validate.salesQuantityIsNumber,
+  validate.salesQuantityIsNot0OrLess,
+  validate.quantityIsNumber,
+  validate.quantityIsNot0OrLess,
+  async (req, res) => {
+    const { id } = req.params;
+    const document = {
+      itensSold: req.body,
+    };
+    await crud.update('sales', id, document);
+    crud.read('id', id, 'sales').then((sale) => res.status(200).json(sale));
+  },
+);
 
-// router.delete('/:id', validations.verifySaleById, async (req, res) => {
-//   const { id } = req.params;
-//   const sale = await crudModel.findById('sales', id);
-//   console.log('sale', sale);
-//   await quantityService.updateProductQuantity(req.method, sale.itensSold);
-//   await crudModel.remove('sales', id);
-//   res.status(200).json(req.sale);
-// });
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const sale = await crud.read('id', id, 'sales');
+  if (!sale) {
+    return res.status(422).json({ err: { code: 'invalid_data', message: 'Wrong id format' } });
+  }
+  await quantityService.updateProductQuantity(req.method, sale.itensSold);
+  await crud.remove('sales', id);
+  res.status(200).json(req.sale);
+});
 
-// module.exports = router;
+module.exports = router;

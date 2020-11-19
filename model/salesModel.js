f22d7bd49bfc2ee4495b50e2ee18276e79b4e506
Kaecio <kaecio@pop-os.localdomain>
2020-11-18 21:48:27 -0300

    Req 5, CC

--- model/salesModel.js
+++ model/salesModel.js
@@ -2,11 +2,11 @@ const connection = require('./connection');
 const { ObjectId } = require('mongodb');
 
 // add produto no db de vendas
-const addSale = async (itensSold) =>{
-    const itens = connection().then((db) => db.collection('sales').insertOne({itensSold}))
-    return itens
+const addSale = async (itensSold) => {
+  const itens = connection().then((db) => db.collection('sales').insertOne({ itensSold }));
+  return itens;
 };
 
 module.exports = {
-    addSale
-}
+  addSale,
+};

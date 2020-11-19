2b5af685f9c2d964f07dfa4ae3732e742477238e
Kaecio <kaecio@pop-os.localdomain>
2020-11-19 13:08:55 -0300

    Req 10, CC

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -11,8 +11,8 @@ const buildResult = (_id, itensSold) => ({
 
 router.put('/:id', async (req, res) => {
   // const filterSale = req.body.filter(
-    (value) => value.quantity <= 0 || typeof value.quantity === 'string',
-  );
+    // (value) => value.quantity <= 0 || typeof value.quantity === 'string',
+  
 
   // validate(res, filterSale);
 

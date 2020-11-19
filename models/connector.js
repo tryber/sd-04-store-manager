ee2efa8e7de21f984b69bd9158b06a044280f049
matheusysd <matheusyurisd@gmail.com>
2020-11-19 01:41:23 -0300

    forgot change mongo url

--- models/connector.js
+++ models/connector.js
@@ -1,9 +1,9 @@
 const mongoClient = require('mongodb').MongoClient;
 
 // LOCAL
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 // EVALUATOR
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 
 const DB_NAME = 'StoreManager';
